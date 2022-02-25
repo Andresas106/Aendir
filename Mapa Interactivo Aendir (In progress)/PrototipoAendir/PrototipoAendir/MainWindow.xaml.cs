4044d33c37b1f00e5f6a0817d0a6797a84213c0e
Andresas <averdes12@gmail.com>
2022-02-26 00:31:39 +0100

    Primera pagina de la aplicacion hecha

--- Mapa Interactivo Aendir (In progress)/PrototipoAendir/PrototipoAendir/MainWindow.xaml.cs
+++ Mapa Interactivo Aendir (In progress)/PrototipoAendir/PrototipoAendir/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrototipoAendir.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,19 @@ namespace PrototipoAendir
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new Mapa();
+            newForm.Show();
+            this.Close();
+            
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
